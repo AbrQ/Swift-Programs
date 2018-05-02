2f91e7c329c8bcde0b87d5bde2477e980525273c
Abraham Quezada <abrahamqt@gmail.com>
2018-05-01 20:21:27 -0500

    New Login project and Optionals created in the class

--- Optionals.playground/Contents.swift
+++ Optionals.playground/Contents.swift
@@ -5,17 +5,28 @@ import UIKit
 struct Book{
     let name: String
     let publicationYear: Int?
+    init?(name: String, publicationYear: Int?){
+        if(publicationYear == 1997){
+            return nil
+        }
+        else{
+            self.name = name
+            self.publicationYear = publicationYear
+        }
+    }
 }
 
 let firstHarryPotter = Book(name: "Harry Potter and the Sorcerer's Stone",publicationYear: 1997)
 
 let secondHarryPotter = Book(name: "Harry Potter and the Chamber of Secrets",publicationYear: 1998)
 
-let newBookHarryPotter = Book(name: "Rebels and Lions", publicationYear: 2020)
+let newBookHarryPotter = Book(name: "Rebels and Lions", publicationYear: nil)
 
 let book = [firstHarryPotter, secondHarryPotter, newBookHarryPotter]
 
-if newBookHarryPotter.publicationYear != nil{
+print(firstHarryPotter)
+
+/*if newBookHarryPotter.publicationYear != nil{
     let actualYear = newBookHarryPotter.publicationYear!
     print(actualYear)
 }
@@ -23,7 +34,54 @@ else{
     print("Próximamente")
 }
 
-var serverResponseCode: Int?
-serverResponseCode = nil
+//optional binding
+if let unwrappedPublicationYear = book[1].publicationYear {
+    print(unwrappedPublicationYear)
+}
+else{
+    print("Proximamente")
+}
+ 
 
-//print("Año de publicacion: \(book[2].publicationYear)")
+let cadena = "1computo2"
+let possibleNumber = Int(cadena)
+if possibleNumber != nil{
+    print(possibleNumber)
+}
+else{
+    print("No fue posible generar un numero")
+}
+*/
+
+
+struct Person{
+    var age: Int
+    var residence: Residence?
+}
+
+struct Residence{
+    var address: Address?
+}
+
+struct Address{
+    var number: String
+    var street: String
+    var apartmentNumber: String?
+}
+
+let persona: Person = Person(age: 12, residence: Residence(address: Address(number: "12", street: "Cien Fuegos", apartmentNumber: "2")))
+//print(persona.residence!.address!.apartmentNumber!)
+if let unwrappedResidencia = persona.residence {
+    if let unwrappedAddres = unwrappedResidencia.address{
+        if let unwrappedApartment = unwrappedAddres.apartmentNumber{
+            print(unwrappedApartment)
+        }
+    }
+}
+
+if let unwrappedEasyApartment = persona.residence?.address?.apartmentNumber{
+    print(unwrappedEasyApartment)
+}
+else{
+    print("No tiene numero de depto")
+}
